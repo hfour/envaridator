58d3ba0bf6ac52f6e05f7559403c225bd956bd71
tinuninu <ichbinjosefk@gmail.com>
2018-11-15 15:05:01 +0100

    adds some comments and few methods

--- index.ts
+++ index.ts
@@ -1,45 +1,70 @@
 import * as toi from '@toi/toi';
 
+interface err {
+  envName: string;
+  toiValErr: toi.ValidationError;
+}
+
 export class Envoi {
-    private environmentVariables: {[name: string]: Envar} = {};
-    private errors: toi.ValidationError[] = [];
+  private registeredEnvVars: { [name: string]: Envar } = {};
+  private failedEnvVars: err[] = [];
 
-    addVariable(name: string, validator: toi.Validator<unknown, unknown>) {
-        if (this.environmentVariables[name] != null) throw new Error (`Variable ${name} already defined.`);
+  registerVariable(name: string, validator: toi.Validator<unknown, unknown>) {
+    // Check if the environment variable exists
+    const value: string | undefined = process.env[name];
+    // if not, throw an error
+    if (!value) throw new Error(`Variable ${name} is undefined!`);
 
-        const ev = new Envar(name, validator);
-        this.environmentVariables[name] = ev;
-        return ev;
+    // Check if the name is already in the registered
+    // environment variables; if not, throw an error.
+    if (this.registeredEnvVars[name] != null) throw new Error(`Variable ${name} already defined!`);
+
+    // Create a new Enver()
+    const envar = new Envar(name, value, validator);
+    // and assign it to the registered environment variables
+    this.registeredEnvVars[name] = envar;
+
+    return envar;
+  }
+
+  validateEnvVars() {
+    Object.keys(this.registeredEnvVars).forEach(name => {
+      try {
+        this.registeredEnvVars[name].validate();
+      } catch (error) {
+        this.failedEnvVars.push({ envName: name, toiValErr: error });
+      }
+    });
+
+    if (this.failedEnvVars.length > 0) {
+      this.showErrors(this.failedEnvVars.length);
     }
+  }
 
-    validate() {
-        Object.keys(this.environmentVariables).forEach(name => {
-            try {
-                this.environmentVariables[name].validate()
-            } catch (e) {
-                this.errors.push(e);
-            }
-        });
-      
-        if (this.errors.length > 0) {
-            this.showErrors();
-            process.exit(1);
-        } else { console.log("All environment variables are valid.");  }
+  showErrors(err: number) {
+    for (let i = 0; i < err; i++) {
+      console.log(
+        `${this.failedEnvVars[i].toiValErr.name}: ${this.failedEnvVars[i].envName}, ${
+          this.failedEnvVars[i].toiValErr.message
+        }`,
+      );
     }
-
-    private showErrors() {
-        console.log(this.errors);
-    }
+    process.exit(1);
+  }
 }
 
 class Envar {
-    constructor(private _name: string, private validator: toi.Validator<unknown, unknown>) {}
+  constructor(private _name: string, private _value: string, private _validator: toi.Validator<unknown, unknown>) {}
 
-    validate() {
-        return this.validator(process.env[this._name]);
-    }
+  validate() {
+    return this._validator(this._value);
+  }
 
-    name() {
-        return this._name;
-    } 
+  get name() {
+    return this._name;
+  }
+
+  get value() {
+    return this._value;
+  }
 }
