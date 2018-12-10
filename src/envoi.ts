2d291dc9b5667dfa683eea9d621411e1af4a2479
Gorgi Kosev <gorgi.kosev@gmail.com>
2018-12-10 17:52:11 +0000

    add description feature

--- src/envoi.ts
+++ src/envoi.ts
@@ -4,60 +4,62 @@
 
 type Validator<T> = (value: string | undefined) => T;
 
-type RegisteredVariable = { 
-  [name: string]: Envar<unknown> 
-}
+type RegisteredVariable = {
+  [name: string]: Envar<unknown>;
+};
 
-type ValidationError = { 
-  message: string
+type ValidationError = {
+  message: string;
 };
 
 /**
- * Envoi handles the registration and validation 
+ * Envoi handles the registration and validation
  * of the environment variables.
  */
 export class Envoi {
   private registeredVariables: RegisteredVariable = {};
 
   private status(failedVariables: EnvarError[]) {
-    let status: string = `The following environment variables are invalid:\n\n`
+    let status: string = `The following environment variables are invalid:\n\n`;
 
     failedVariables.forEach(envar => {
-      status += `${envar.errorVariableName} - ${envar.errorMessage}\n`
-    })
+      status += `${envar.errorVariableName} - ${envar.errorMessage}\n`;
+    });
 
     return status;
   }
 
   /**
-   * register is a function that tries to extract the 
-   * environment variable value from the given environment 
-   * variable name, if everything goes correctly, 
+   * register is a function that tries to extract the
+   * environment variable value from the given environment
+   * variable name, if everything goes correctly,
    * it will return a new {@link Envar}.
-   * 
+   *
    * @param name the name of the environment variable
    * @param validator the actual validator (e.g. toi.required().and(toi.str.is()))
+   * @param description string describing the variable
    */
-  register<T>(name: string, validator: Validator<T>) {
-    if (this.registeredVariables[name] != null) throw new Error(`Variable ${name} already defined!`);
+  register<T>(name: string, validator: Validator<T>, description: string) {
+    if (this.registeredVariables[name] != null)
+      throw new Error(`Variable ${name} already defined!`);
 
-    const envar = new Envar(name, validator);
+    const envar = new Envar(name, validator, description);
     this.registeredVariables[name] = envar;
 
     return envar;
   }
 
   /**
-   * Iterates over {@link registeredVariables} and 
+   * Iterates over {@link registeredVariables} and
    * calls the validator function on every {@link Envar}.
-   * If the variables are successfully validated, it returns 
+   * If the variables are successfully validated, it returns
    * {@link registeredVariables}.
-   * 
-   * In case one or more variables fail their validation, 
+   *
+   * In case one or more variables fail their validation,
    * it returns {@link failedVariables} instead.
    */
   validate() {
-    let failedVariables: EnvarError[] = []
+    let failedVariables: EnvarError[] = [];
 
     Object.keys(this.registeredVariables).forEach(name => {
       try {
@@ -69,7 +71,16 @@ export class Envoi {
 
     if (failedVariables.length > 0) {
       throw new Error(this.status(failedVariables));
-    };
+    }
+  }
+
+  /**
+   * Describe all registered environment variables
+   */
+  describeAll() {
+    return Object.keys(this.registeredVariables)
+      .map(name => `${name} - ${this.registeredVariables[name].description}`)
+      .join('\n');
   }
 }
 
@@ -79,16 +90,20 @@ export class Envoi {
  * a {@link Envar} object for later use.
  */
 export class Envar<T> {
-  constructor(private _name: string, private _validator: Validator<T>) {}
+  constructor(
+    private _name: string,
+    private _validator: Validator<T>,
+    private _description: string
+  ) {}
 
-  private cache: {empty: true} | {value: T} = { empty: true };
+  private cache: { empty: true } | { value: T } = { empty: true };
 
   private validate() {
-    if ("empty" in this.cache) {
+    if ('empty' in this.cache) {
       const value: string | undefined = process.env[this._name];
-      this.cache = {value: this._validator(value)};
+      this.cache = { value: this._validator(value) };
     }
-  
+
     return this.cache.value;
   }
 
@@ -98,6 +113,10 @@ export class Envar<T> {
 
   get value() {
     return this.validate();
+  }
+
+  get description() {
+    return this._description;
   }
 }
 
