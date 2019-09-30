510a7521bc4d40fdce62b0d31e1170e94e30d96c
Ivan Vasilov <vasilov.ivan@gmail.com>
2019-09-30 14:40:02 +0200

    Update the tests for registerPostValidation.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -52,7 +52,7 @@ it('should not throw if all of the variables and rules are successfully validate
     'A correct url for this app',
   );
 
-  envaridator.registerPostValidation('EXAMPLE_RULE', () => {}, 'Empty post validation rule');
+  envaridator.registerPostValidation('Empty post validation rule', () => {});
 
   // plain return is enough because the test if it not throws if everything is alright.
   expect(() => envaridator.validate()).not.toThrow();
@@ -82,37 +82,22 @@ it('should throw if the validation fails', () => {
 
 it('should throw if a post validation rule fails', () => {
   const envaridator = new Envaridator();
-  envaridator.registerPostValidation(
-    'THROW_RULE',
-    () => {
-      throw new Error('This rule should throw');
-    },
-    'This rule will throw when the envaridator validate is run',
-  );
+  envaridator.registerPostValidation('This rule will throw when the envaridator validate is run', () => {
+    throw new Error('This rule should throw');
+  });
 
-  expect(() => envaridator.validate()).toThrow(
-    'The following environment variables are invalid:\n\nTHROW_RULE - This rule should throw',
-  );
-});
-
-it('should throw if a post validation rule already exists', () => {
-  const envaridator = new Envaridator();
-  envaridator.registerPostValidation('DEFINED_RULE', () => {}, '');
-
-  expect(() => envaridator.registerPostValidation('DEFINED_RULE', () => {}, '')).toThrow(
-    'Post validation rule DEFINED_RULE already defined!',
-  );
+  expect(() => envaridator.validate()).toThrow('The following validation rules are invalid:\n\nThis rule should throw');
 });
 
 it('should describe all registered variables and rules', () => {
   const envaridator = new Envaridator();
   envaridator.register('VAR_1', toi.required(), 'The first variable. Must be configured correctly.');
   envaridator.register('VAR_2', toi.required(), 'The second variable. Must be configured correctly.');
-  envaridator.registerPostValidation('RULE_1', () => {}, 'The first rule. Must be configured correctly.');
+  envaridator.registerPostValidation('The first rule. Must be configured correctly.', () => {});
   expect(envaridator.describeAll()).toMatchInlineSnapshot(`
 "VAR_1 - The first variable. Must be configured correctly.
 VAR_2 - The second variable. Must be configured correctly.
-RULE_1 - The first rule. Must be configured correctly."
+The first rule. Must be configured correctly."
 `);
 });
 
@@ -131,10 +116,10 @@ it('should describe all registered variables with some markdown formatting', () 
 
 it('should describe all rules with some markdown formatting', () => {
   const envaridator = new Envaridator();
-  envaridator.registerPostValidation('RULE_1', () => {}, 'The first rule. Must be configured correctly.');
+  envaridator.registerPostValidation('The first rule. Must be configured correctly.', () => {});
   expect(envaridator.describeAllMarkdown()).toMatchInlineSnapshot(`
 "#Post validation rules
-**RULE_1** - The first rule. Must be configured correctly."
+The first rule. Must be configured correctly."
 `);
 });
 
@@ -143,13 +128,13 @@ it('should describe all registered variables and rules with some markdown format
   envaridator.register('VAR_1', toi.required(), 'The first variable. Must be configured correctly.');
   envaridator.register('VAR_2', toi.required(), 'The second variable. Must be configured correctly.');
   envaridator.register('VAR_3', toi.required(), 'The third variable. Must be configured correctly.');
-  envaridator.registerPostValidation('RULE_1', () => {}, 'The first rule. Must be configured correctly.');
+  envaridator.registerPostValidation('The first rule. Must be configured correctly.', () => {});
   expect(envaridator.describeAllMarkdown()).toMatchInlineSnapshot(`
 "#Variables
 **VAR_1** - The first variable. Must be configured correctly.
 **VAR_2** - The second variable. Must be configured correctly.
 **VAR_3** - The third variable. Must be configured correctly.
 #Post validation rules
-**RULE_1** - The first rule. Must be configured correctly."
+The first rule. Must be configured correctly."
 `);
 });
